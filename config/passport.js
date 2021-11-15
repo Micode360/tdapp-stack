97c0fe7c2916d0c044e343d3f6820b62ff60b1e4
Micode <44612265+Micode360@users.noreply.github.com>
2021-11-15 11:08:33 +0100

    Removed comment.

--- config/passport.js
+++ config/passport.js
@@ -3,49 +3,6 @@ const registerationModel = require('../models/register');
 const bcrypt = require('bcrypt');
 
 
-
-// module.exports = function (passport) {
-
-// passport.use(new localStrategy({ usernameField: 'username' }, (username, password, done) => {
-//       console.log({
-//         username: username,
-//         password: password
-//       });
-//     registerationModel.findOne({ username: username }, function(err, user) {
-//       console.log({
-//         user: user
-//       });
-      
-//       if(!user) done(null, false, { message: "username does not exist" } ); 
-
-//       if (!user.password) {
-//       return done(null, false, { msg: 'Your account was registered using a sign-in provider. To enable password login, sign in using a provider, and then set a password under your user profile.' });
-//        }
-      
-//       bcrypt.compare(password, user.password, (err, match) => {
-//           if(err) done(null, false, { message: 'Invalid Password'})
-
-//           if(match) return done(null, user)
-//           else return done(null, false, { message: 'Invalid Password'})
-          
-//       });
-//     });
-//   }
-// ));
-
-// passport.serializeUser((user, done) => {
-//   done(null, user.id);
-// });
-
-
-// passport.deserializeUser((id, done) => {
-//   User.findById(id).then((user) => {
-//          done(null, user);
-//   });
-//  });
-
-// }
-
 module.exports = function(passport) {
   passport.use(
     new localStrategy({ usernameField: 'username' }, (username, password, done) => {
